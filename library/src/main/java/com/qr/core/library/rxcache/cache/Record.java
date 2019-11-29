ef63dd8dcb4cb9717a554622352ef437a8630f1d
锐气波发 <973440230@qq.com>
2019-11-29 10:01:22 +0800

    Record增加空的构造函数

--- library/src/main/java/com/qr/core/library/rxcache/cache/Record.java
+++ library/src/main/java/com/qr/core/library/rxcache/cache/Record.java
@@ -1,14 +1,17 @@
 package com.qr.core.library.rxcache.cache;
 
-public final class Record<T>{
+public final class Record<T> {
     // 数据
-    private final T data;
+    private T data;
     // 存活时间
     private long survivalTime;
     // 保存时的时间
-    private final long persistedTime;
+    private long persistedTime;
 
-    public Record(T data, long survivalTime,long persistedTime){
+    public Record() {
+    }
+
+    public Record(T data, long survivalTime, long persistedTime) {
         this.data = data;
         this.survivalTime = survivalTime;
         this.persistedTime = persistedTime;
@@ -17,13 +20,26 @@ public final class Record<T>{
     public long getPersistedTime() {
         return persistedTime;
     }
+
+    public void setPersistedTime(long persistedTime) {
+        this.persistedTime = persistedTime;
+    }
+
+    public void setSurvivalTime(long survivalTime) {
+        this.survivalTime = survivalTime;
+    }
+
+
     public long getSurvivalTime() {
         return survivalTime;
     }
-    public void setSurvivalTime(long survivalTime){
-        this.survivalTime = survivalTime;
-    }
+
     public T getData() {
         return data;
     }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
 }
